856abe079cfaf59e780cb671279d75e34b8e5c3f
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-01-19 12:01:30 +0100

    install handelbars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
+const { engine } = require('express-handlebars');
+
+app.use('/static', express.static('public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
