e869ae321afd63987e8bb894388eb495cf329562
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-01-24 23:33:53 +0100

    set interval

--- src/public/static/js/index.js
+++ src/public/static/js/index.js
@@ -5,40 +5,42 @@ const dataSet = async function getData() {
   return axios.get('/coco-data');
 };
 
+const width = 800;
+const height = 600;
+const margin = {
+  top: 20, right: 20, bottom: 100, left: 100,
+};
+const graphWidth = width - margin.left - margin.right;
+const graphHeight = height - margin.top - margin.bottom;
+
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const gXAxis = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`);
+
+const gYAxis = graph.append('g');
+
+const y = d3.scaleLinear()
+  .range([graphHeight, 0]);
+
+const x = d3.scaleBand()
+  .range([0, 500])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
 async function drawChart() {
   const data = await dataSet();
-  const width = 800;
-  const height = 600;
-  const margin = {
-    top: 20, right: 20, bottom: 100, left: 100,
-  };
-  const graphWidth = width - margin.left - margin.right;
-  const graphHeight = height - margin.top - margin.bottom;
 
-  const svg = d3.select('.canvas')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-
-  const graph = svg.append('g')
-    .attr('width', graphWidth)
-    .attr('height', graphHeight)
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-  const gXAxis = graph.append('g')
-    .attr('transform', `translate(0, ${graphHeight})`);
-
-  const gYAxis = graph.append('g');
-
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data.data, (d) => d.sum)])
-    .range([graphHeight, 0]);
-
-  const x = d3.scaleBand()
-    .domain(data.data.map((item) => item.type_name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
+  y.domain([0, d3.max(data.data, (d) => d.sum)]);
+  x.domain(data.data.map((item) => item.type_name));
 
   const rects = graph.selectAll('rect')
     .data(data.data);
@@ -49,9 +51,23 @@ async function drawChart() {
     .attr('x', (d) => x(d.type_name))
     .attr('y', (d) => y(d.sum));
 
+  rects.exit()
+    .transition()
+    .duration(1000)
+    .attr('width', 0)
+    .remove();
+
   rects.enter()
     .append('rect')
     .attr('class', 'bar-rect')
+    .attr('width', x.bandwidth)
+    .attr('height', (d) => graphHeight - y(d.sum))
+    .attr('x', (d) => x(d.type_name))
+    .attr('y', (d) => y(d.sum))
+    .merge(rects)
+    .transition()
+    .duration(1000)
+    .delay(1000)
     .attr('width', x.bandwidth)
     .attr('height', (d) => graphHeight - y(d.sum))
     .attr('x', (d) => x(d.type_name))
@@ -66,13 +82,14 @@ async function drawChart() {
   gYAxis.call(yAxis);
 
   gXAxis.selectAll('text')
-    .style('font-size', 14);
+    .style('font-size', 11);
 
   gYAxis.selectAll('text')
-    .style('font-size', 14);
+    .style('font-size', 11);
 }
 
 drawChart();
-// setTimeout(function(){
-//   window.location.reload(1);
-// }, 10000);
+
+window.setInterval(() => {
+  drawChart();
+}, 10000);
